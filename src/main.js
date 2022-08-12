0cfe67d2eeef7069e76cd7d20e91d77599076f3a
Alexander Lavrov <alexloverow@gmail.com>
2022-08-12 19:45:36 +0300

    1.11. Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import FilterView from './view/filter-view';
-import { render } from './render';
+import FilterView from './view/filter-view.js';
+import { render } from './render.js';
+import BoardPresenter from './presenter/presenter.js';
 
+const boardLocation = document.querySelector('.trip-events');
 const headerElement = document.querySelector('.page-header');
 const siteHeaderElement = headerElement.querySelector('.trip-controls__filters');
+const boardPresenter = new BoardPresenter();
 
 render(new FilterView(), siteHeaderElement);
+
+boardPresenter.init(boardLocation);
